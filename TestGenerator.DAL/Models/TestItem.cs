eec8f51612d48f29bb9aebed23263aa64ecb46dc
mnegrisan <Mihai.Negrisan@endava.com>
2023-04-06 14:10:32 +0300

    Created TestItem Model

---
+++ TestGenerator.DAL/Models/TestItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TestGenerator.DAL.Models;
+
+public class TestItem
+{
+    public int TestItemId { get; set; }
+    public string? Question { get; set; }
+    public IEnumerable<string>? Answers { get; set; }
+    public int CorrectAnswerIndex { get; set; }
+}
