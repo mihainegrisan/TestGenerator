de0892b67e37d78f033de47af4f4b0bf05b872a0
Negrisan Mihai <mihainegrisan1@gmail.com>
2023-04-06 21:14:56 +0300

    Adjusted migrations to the new namespace for Data folder

--- TestGenerator.DAL/Data/Migrations/20230406114413_Added Test, Question and Answer models.Designer.cs
+++ TestGenerator.DAL/Data/Migrations/20230406114413_Added Test, Question and Answer models.Designer.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using TestGenerator.DAL.Data;
 using TestGenerator.Web.Data;
 
 #nullable disable
