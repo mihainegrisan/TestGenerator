c8327a07e0ad8c704a0fd4468066bb8678d65566
Negrisan Mihai <mihainegrisan1@gmail.com>
2023-04-08 10:09:35 +0300

    Added repository for Test model

---
+++ TestGenerator.Web/Repositories/ITestRepository.cs
@@ -0,0 +1,11 @@
+﻿using TestGenerator.DAL.Models;
+
+namespace TestGenerator.Web.Repositories;
+
+public interface ITestRepository
+{
+  Task<Test> AddTest(Test test);
+  Task<Test> GetTest(int id);
+  Task<Test> UpdateTest(Test test);
+  Task<bool> DeleteTest(int id);
+}
---
+++ TestGenerator.Web/Repositories/TestRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestGenerator.DAL.Data;
+using TestGenerator.DAL.Models;
+
+namespace TestGenerator.Web.Repositories;
+
+public class TestRepository : ITestRepository
+{
+  private readonly ApplicationDbContext _dbContext;
+
+  public TestRepository(ApplicationDbContext dbContext)
+  {
+    _dbContext = dbContext;
+  }
+
+  public async Task<Test> AddTest(Test test)
+  {
+    _dbContext.Tests?.Add(test);
+    await _dbContext.SaveChangesAsync();
+    return test;
+  }
+
+  public async Task<Test> GetTest(int id)
+  {
+    return await _dbContext.Tests.FindAsync(id);
+  }
+
+  public async Task<Test> UpdateTest(Test test)
+  {
+    _dbContext.Entry(test).State = EntityState.Modified;
+    await _dbContext.SaveChangesAsync();
+    return test;
+  }
+
+  public async Task<bool> DeleteTest(int id)
+  {
+    var test = await _dbContext.Tests.FindAsync(id);
+    if (test == null)
+      return false;
+
+    _dbContext.Tests.Remove(test);
+    await _dbContext.SaveChangesAsync();
+    return true;
+  }
+}
