a6d717cbedf7fa012560f98f7d11abef1fc893ba
Negrisan Mihai <mihainegrisan1@gmail.com>
2023-04-08 10:09:09 +0300

    Renamed test controller

--- TestGenerator.Web/Controllers/TestController.cs
+++ TestGenerator.Web/Controllers/TestController.cs
@@ -1,28 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TestGenerator.DAL.Data;
+using TestGenerator.DAL.Models;
 using TestGenerator.Web.Services;
 
 namespace TestGenerator.Web.Controllers
 {
-  public class TestsController : Controller
+  public class TestController : Controller
   {
     private readonly ApplicationDbContext _context;
     private readonly IFileProcessor _fileProcessor;
 
-    public TestsController(ApplicationDbContext context, IFileProcessor fileProcessor)
+    public TestController(ApplicationDbContext context, IFileProcessor fileProcessor)
     {
       _context = context;
       _fileProcessor = fileProcessor;
     }
 
-    // GET: 
-    public IActionResult GenerateTest()
+    // GET: Test/Create
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    // POST: Test/AddTest
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Add(Test test)
+    {
+      if (ModelState.IsValid)
+      {
+        _context.Tests.Add(test);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
+      return View(test);
+    }
+
+
+    // GET: Test/Generate
+    public IActionResult Generate()
     {
       return View();
     }
 
     [HttpPost]
-    public Task<IActionResult> GenerateTest(IFormFile file)
+    public Task<IActionResult> Generate(IFormFile file)
     {
       //bool isUploaded = await _fileProcessor.UploadFile(file);
 
