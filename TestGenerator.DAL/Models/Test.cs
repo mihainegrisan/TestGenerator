9e17b9f944d1d55d5795d39ee7408b6de679a349
Negrisan Mihai <mihainegrisan1@gmail.com>
2023-04-09 14:50:42 +0300

    Added data annotation for Test model

--- TestGenerator.DAL/Models/Test.cs
+++ TestGenerator.DAL/Models/Test.cs
@@ -14,11 +14,13 @@ public class Test
     public string? Description { get; set; }
 
     [Required]
-    public List<Question>? Questions { get; set; }
+    public List<Question>? Questions { get; set; } = new();
 
     [Required]
+    [Range(1, 50)]
     public int NumberOfQuestions { get; set; }
 
     [Required]
+    [Range(2, 10)]
     public int NumberOfAnswersPerQuestion { get; set; }
 }
