2ca51be16ca9d55dda43b9c34fef07de7df4fde7
mnegrisan <mihainegrisan1@gmail.com>
2023-04-11 11:08:32 +0300

    Separated normal Test controller and views from the ones using ChatGPT

--- TestGenerator.Web/Controllers/TestController.cs
+++ TestGenerator.Web/Controllers/TestController.cs
@@ -2,19 +2,16 @@
 using Microsoft.EntityFrameworkCore;
 using TestGenerator.DAL.Models;
 using TestGenerator.Web.Repositories;
-using TestGenerator.Web.Services;
 
 namespace TestGenerator.Web.Controllers
 {
     public class TestController : Controller
     {
         private readonly ITestRepository _testRepository;
-        private readonly IFileProcessor _fileProcessor;
 
-        public TestController(ITestRepository testRepository, IFileProcessor fileProcessor)
+        public TestController(ITestRepository testRepository)
         {
             _testRepository = testRepository;
-            _fileProcessor = fileProcessor;
         }
 
         public async Task<IActionResult> Index()
@@ -167,30 +164,6 @@ namespace TestGenerator.Web.Controllers
             await _testRepository.DeleteTestAsync(id);
 
             return RedirectToAction("Index");
-        }
-
-        // GET: Test/Generate
-        public IActionResult Generate()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public Task<IActionResult> Generate(IFormFile file)
-        {
-            //bool isUploaded = await _fileProcessor.UploadFile(file);
-
-            //if (!isUploaded)
-            //{
-            //  return View();
-            //}
-
-            //TempData["Message"] = "File uploaded successfully";
-            // From now on, work with the saved file
-
-            var text = _fileProcessor.GetTextFromFile(file);
-
-            return Task.FromResult<IActionResult>(View());
         }
     }
 }
---
+++ TestGenerator.Web/Controllers/TestGeneratorController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestGenerator.Web.Repositories;
+using TestGenerator.Web.Services;
+
+namespace TestGenerator.Web.Controllers
+{
+    public class TestGeneratorController : Controller
+    {
+        private readonly ITestRepository _testRepository;
+        private readonly IFileProcessor _fileProcessor;
+
+        public TestGeneratorController(ITestRepository testRepository, IFileProcessor fileProcessor)
+        {
+            _testRepository = testRepository;
+            _fileProcessor = fileProcessor;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Test/Generate
+        public IActionResult Generate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public Task<IActionResult> Generate(IFormFile file)
+        {
+            //bool isUploaded = await _fileProcessor.UploadFile(file);
+
+            //if (!isUploaded)
+            //{
+            //  return View();
+            //}
+
+            //TempData["Message"] = "File uploaded successfully";
+            // From now on, work with the saved file
+
+            var text = _fileProcessor.GetTextFromFile(file);
+
+            return Task.FromResult<IActionResult>(View());
+        }
+    }
+}
