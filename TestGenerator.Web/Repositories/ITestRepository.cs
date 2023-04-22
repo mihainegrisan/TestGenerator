0a9044460f745e34278c9ee8236af1976af26352
Mihai Negrisan <mihainegrisan1@gmail.com>
2023-04-22 14:44:42 +0300

    Removed unneeded methods

--- TestGenerator.Web/Repositories/ITestRepository.cs
+++ TestGenerator.Web/Repositories/ITestRepository.cs
@@ -4,11 +4,13 @@ namespace TestGenerator.Web.Repositories;
 
 public interface ITestRepository
 {
-    Task<Test> FindAsync(int? id);
     Task<Test> AddTestAsync(Test test);
+
     Task<Test> GetTestAsync(int? id);
+
     Task<List<Test>> GetTestsAsync();
+
     Task<Test> UpdateTestAsync(Test test);
+
     Task<bool> DeleteTestAsync(int id);
-    bool TestExists(int id);
 }
